3cd3efc627ef4b68b2aeb8a673b3c5f41e5d53cb
AbhiCN <93698894+nowucme2@users.noreply.github.com>
2026-03-13 12:46:33 +0530

    Update menu.py

--- ui/menu.py
+++ ui/menu.py
@@ -1,13 +1,40 @@
 from rich.console import Console
 from rich.prompt import Prompt
 
+from core.entry_manager import add_entry_to_table
+from core.viewer import view_specific_table
 from core.table_manager import create_table
-from core.entry_manager import add_entry
-from core.viewer import view_table
 from reports.excel_export import export_excel
 from reports.html_dashboard import generate_dashboard
 
 console = Console()
+
+
+def tracker_menu(table):
+
+    while True:
+
+        console.print(f"""
+
+[bold green]{table.upper()}[/bold green]
+
+1 Add Entry
+2 View Entries
+0 Back
+
+""")
+
+        c = Prompt.ask("Select")
+
+        if c == "1":
+            add_entry_to_table(table)
+
+        elif c == "2":
+            view_specific_table(table)
+
+        elif c == "0":
+            break
+
 
 def start_menu():
 
@@ -17,7 +44,7 @@ def start_menu():
 
 [bold cyan]
 
-CN JOURNAL FORGE
+🐺 CN JOURNAL FORGE
 
 Built-in Trackers
 1 VAPT Projects
@@ -30,13 +57,9 @@ Built-in Trackers
 Custom
 7 Create Custom Table
 
-Actions
-8 Add Entry
-9 View Table
-
 Reports
-10 Export Excel
-11 HTML Dashboard
+8 Export Excel
+9 HTML Dashboard
 
 0 Exit
 
@@ -46,19 +69,31 @@ Reports
 
         c = Prompt.ask("Select")
 
-        if c == "7":
+        if c == "1":
+            tracker_menu("vapt_projects")
+
+        elif c == "2":
+            tracker_menu("redteam_projects")
+
+        elif c == "3":
+            tracker_menu("git_tracker")
+
+        elif c == "4":
+            tracker_menu("study_journal")
+
+        elif c == "5":
+            tracker_menu("timetable")
+
+        elif c == "6":
+            tracker_menu("personal_secure")
+
+        elif c == "7":
             create_table()
 
         elif c == "8":
-            add_entry()
-
-        elif c == "9":
-            view_table()
-
-        elif c == "10":
             export_excel()
 
-        elif c == "11":
+        elif c == "9":
             generate_dashboard()
 
         elif c == "0":
